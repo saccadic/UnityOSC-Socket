480c1b86da1fdad4ab15917cb6b45e8724c7a0eb
Savant_Cat <scanf@me.com>
2015-02-21 16:13:28 +0900

    初コミット

---
+++ node/udp.js
@@ -0,0 +1,25 @@
+var PORT = 8085;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+
+var fs = require('fs');
+server.on('message', function (message, remote) {
+    var buf = new Buffer(message);
+    console.log(message +"");
+    console.log(buf);
+    
+    
+
+fs.writeFile('data_u2.txt', buf);
+
+});
+
+server.bind(PORT, HOST);
